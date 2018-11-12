4624b7b50efd38d990cf8218ed7c5baba582a59f
TerrySmithMBA <30348725+TerrySmithMBA@users.noreply.github.com>
2018-11-11 20:38:06 -0500

    Add files via upload

---
+++ get-ProcessesThatStandOut.ps1
@@ -0,0 +1,78 @@
+﻿<#
+.SYNOPSIS
+    Outputs the following:
+        1. Process Name
+        2. Path based on Get-Database.ps1 script [Should be a from a master image]
+        3. Actual Location of Process
+        4. If locations do not match, a notification appears next.
+        5. Name of Parent Process
+        
+.EXAMPLE
+    Process: svchost
+    Correct Location: c:\windows\system32\svchost.exe 
+    Actual Location: c:\users\badguy\downloads\svchost.exe
+    svchost in wrong location!
+    Parent Process: bad.exe
+
+.USAGE
+    crystalblue.ps1
+
+
+
+#>
+
+
+    $locations = gc "C:\Users\abcd\Desktop\lisa image.txt"
+    Write-Host "Checking processes..."
+    foreach($proc in get-process){
+        $name = $proc.Name
+        $path = $proc.Path
+    
+        $parentprocessid = (get-wmiobject win32_process | ? {$_.name -eq "$name`.exe"}).parentprocessid
+        $parentprocess = (get-wmiobject win32_process | ? {$_.processid -eq $parentprocessid}).name
+        
+        if (($locations | %{$_ -match $name}) -contains $true) {
+
+            foreach ($line in $locations){
+                if ($line -like "$name`#*"){
+           
+                    $correctlocation = $line.split("#")[1]
+                
+                    if ($line.split("#")[1]){
+                    
+                        if ($path -ne $line.split("#")[1]){
+                            Write-Host "$name in wrong location! `n"
+                        }
+                    }
+                    else {
+                    
+                    }
+
+                    if ($parentprocess){
+                    
+                    }
+                    else {
+                    
+                    }
+                break
+                }
+       
+            }
+        }
+        else {
+            $company = $proc.company
+            $starttime = $proc.starttime
+            Write-Host "Additional Process Running: $name"
+            Write-Host "Start time: $starttime"
+            if ($company){
+                Write-Host "Company: $company"
+            }
+            else {
+                Write-Host "No company information given"
+
+            }
+            Write-Host "`n"
+        }
+        
+    }
+

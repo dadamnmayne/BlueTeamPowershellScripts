6df71f4ac780f4bdfc7f8ea602db26aef41d56d6
TerrySmithMBA <30348725+TerrySmithMBA@users.noreply.github.com>
2019-01-13 22:40:29 -0500

    Add files via upload

---
+++ get-UpdatingNetStat.ps1
@@ -0,0 +1,22 @@
+﻿
+Write-Host "Monitoring Connections"
+
+while ($true){
+    $current = get-nettcpconnection | ? {$_.state -eq "established"} | out-string
+    sleep(1)
+    $compare = get-nettcpconnection | ? {$_.state -eq "established"} | out-string
+    if ($compare){
+        $update = compare-object ($compare) ($current)
+        if ($update -ne $null){
+            clear
+            $compare
+        }
+        $compare = $current
+        sleep(1)
+    }
+    else {
+
+        $compare = $current
+    
+    }
+}
